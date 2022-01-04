7871489470a286fb8532bfce1371305d0319a795
Sid1608 <akarsiddharth@gmail.com>
2022-01-04 10:52:39 +0530

    add Item functionality added

--- client/src/modules/Inventory/components/AddItemsModal/AddItemsModal.js
+++ client/src/modules/Inventory/components/AddItemsModal/AddItemsModal.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import "./AddItemsModal.css"
-import axios from "axios"
+import "./AddItemsModal.css";
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
@@ -17,16 +17,17 @@ const style = {
   borderRadius: 2
 };
 
-export default function AddItemsModal() {
+export default function AddItemsModal(props) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [item, setItem] = React.useState({
     item_name: "",
-    expected_cost:0,
-    item_count: 1,
     item_description: "",
+    expected_cost: 0,
+    item_count: 0,
   });
+
 
   const handleChange = (event) => {
     //destructuring
@@ -35,20 +36,34 @@ export default function AddItemsModal() {
       return { ...prev, [name]: value };
     });
   };
-  const addItem=(event)=>{
-    axios.post("http://localhost:8080/admin/addItem",item).then(response=>{
-      alert("item added successfully");
-      setOpen(false)
-      console.log(response)
-    }).catch(err=>{
-      console.log(err);
-    })
-  }
+
+  const onSubmit = (event) => {
+    /*const newItem = {
+      item_name: item.item_name,
+      item_description: item.item_description,
+      expected_cost: item.expected_cost,
+      item_count: item.item_count,
+    };*/
+    axios.post("http://localhost:8080/admin/addItem", item).then((res) => {
+      console.log(res);
+      props.go(res.data.item);
+    });
+    //props.go(n);
+    setItem({
+      item_name: "",
+      item_description: "",
+      expected_cost: 0,
+      item_count: 0,
+    });
+    handleClose();
+    event.preventDefault();
+  };
+
   return (
     <div>
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-black-700 rounded mx-1"
-        style={{height: "2.5rem"}}
-         onClick={handleOpen}>
+        style={{ height: "2.5rem" }}
+        onClick={handleOpen}>
         Add Items
       </button>
       <Modal
@@ -58,39 +73,39 @@ export default function AddItemsModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-                Add Items
-            </Typography>
-            <form className="w-full grid grid-cols-2 mt-4">
-                <div className='px-1'>
-                    <label htmlFor="new" className='label'>Item Name</label>
-                    <div className='w-full flex p-2 password-container'>
-                        <input type="text" name="item_name" id="new" className='password'/>
-                    </div>
-                </div>
-                <div className='px-1'>
-                    <label htmlFor="current" className='label'>Description</label>
-                    <div className='w-full flex p-2 password-container'>
-                        <input type="text" name="item_description" id="current" className='password'/>
-                    </div>
-                </div>
-                <div className='px-1'>
-                    <label htmlFor="new" className='label'>Cost</label>
-                    <div className='w-full flex p-2 password-container'>
-                        <input type="text" name="expected_cost" id="new" className='password'/>
-                    </div>
-                </div>
-                <div className='px-1'>
-                    <label htmlFor="current" className='label'>Quantity</label>
-                    <div className='w-full flex p-2 password-container'>
-                        <input type="text" name="item_count" id="current" className='password'/>
-                    </div>
-                </div>
-            </form>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-black-700 rounded"
-                onClick={addItem}>
-                    Add Item
-            </button>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Items
+          </Typography>
+          <form className="w-full grid grid-cols-2 mt-4">
+            <div className='px-1'>
+              <label htmlFor="new" className='label'>Item Name</label>
+              <div className='w-full flex p-2 password-container'>
+                <input type="text" id="new" className='password' name="item_name" value={item.item_name} onChange={handleChange} />
+              </div>
+            </div>
+            <div className='px-1'>
+              <label htmlFor="current" className='label'>Description</label>
+              <div className='w-full flex p-2 password-container'>
+                <input type="text" id="current" className='password' name="item_description" value={item.item_description} onChange={handleChange} />
+              </div>
+            </div>
+            <div className='px-1'>
+              <label htmlFor="new" className='label'>Cost</label>
+              <div className='w-full flex p-2 password-container'>
+                <input type="text" id="new" className='password' name="expected_cost" value={item.expected_cost} onChange={handleChange} />
+              </div>
+            </div>
+            <div className='px-1'>
+              <label htmlFor="current" className='label'>Quantity</label>
+              <div className='w-full flex p-2 password-container'>
+                <input type="text" id="current" className='password' name="item_count" value={item.item_count} onChange={handleChange} />
+              </div>
+            </div>
+          </form>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-black-700 rounded"
+            onClick={onSubmit}>
+            Add Item
+          </button>
         </Box>
       </Modal>
     </div>
--- client/src/modules/Inventory/pages/inventory/inventory.js
+++ client/src/modules/Inventory/pages/inventory/inventory.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../../common_styles.css";
-import {SearchIcon} from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/solid"
 import Modal from "../../components/AddItemsModal/AddItemsModal"
 import "./Inventory.css"
 import axios from "axios";
@@ -16,75 +16,90 @@ const Inventory = () => {
     })
   }, [])
 
-
-  async function handleChange(event){
-    try{
+  function go(item) {
+    //const { name, value } = event.target;
+    setInventoryData((prev) => {
+      return [...prev, item];
+    });
+  }
+  async function handleChange(event) {
+    try {
       axios.get(`http://localhost:8080/admin/${event.target.value}`).then(res => {
         var inventoryItems = res.data.items
         setInventoryData(inventoryItems)
         console.log(inventoryItems)
       })
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
 
-  
-  function returnAllInventoryData(inventoryData){
-    if(!inventoryData){
+
+  function returnAllInventoryData(inventoryData) {
+    if (!inventoryData) {
       return <h1>No Items Present</h1>
     }
-    else{
+    else {
       return inventoryData.map((item, i) => (
         <tr key={item._id}>
-          <td>{i+1}</td>
+          <td>{i + 1}</td>
           <td>{item.item_name}</td>
           <td>{item.item_description}</td>
-          <td>{item.item_count}</td>                   
+          <td>{item.item_count}</td>
         </tr>
       ))
     }
   }
 
+  function searchByName() {
+    let x = document.getElementById("item").value;
+
+    axios.get(`http://localhost:8080/admin/searchItem/${x}`)
+      .then(response => setInventoryData(response.data))
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-        <div className="main-body">
-          <h1>Inventory</h1>
-          <div className="content-box">
-            <div style={{display: 'flex', width: "100%", justifyContent: "center",marginBottom:"2rem"}}>
-              <div>
-                <input className="searchInput" placeholder ="Enter ItemID"/>
-              </div>
-              <div className="ml-2" style={{display:"flex"}}>
-                <button type="submit" className="btn btn-primary searchButton">
-                  <SearchIcon className="h-4 mr-1"/>
-                  Search
-                </button>
-              </div>
-              <div className="sortItems">
-                <select name="itemAge" onChange={handleChange}>
-                  <option value='inventory' default
-                    >All Items</option>
-                  <option value='fsn1'
-                    >More than 5 years</option>
-                  <option value='fsn2'
-                    >More than 10 years</option>
-                  <option value='C'
-                    >C Items/Cheap Items</option>
-                  <option value='B'
-                    >B Items/Mid Ranged Items</option>
-                  <option value='A'
-                    >A Items/Expensive Items</option>
-                </select>
-              </div>
-              <Modal/> 
-            </div>
-            <div className="card">
-            <div className="table-responsive">
-              {inventoryData?.length > 0 ?
+    <div className="main-body">
+      <h1>Inventory</h1>
+      <div className="content-box">
+        <div style={{ display: 'flex', width: "100%", justifyContent: "center", marginBottom: "2rem" }}>
+          <div>
+            <input className="searchInput" placeholder="Enter Item Name(exact same)" id="item" />
+          </div>
+          <div className="ml-2" style={{ display: "flex" }}>
+            <button type="submit" className="btn btn-primary searchButton" onClick={searchByName}>
+              <SearchIcon className="h-4 mr-1" />
+              Search
+            </button>
+          </div>
+          <div className="sortItems">
+            <select name="itemAge" onChange={handleChange}>
+              <option value='inventory' default
+              >All Items</option>
+              <option value='fsn1'
+              >More than 5 years</option>
+              <option value='fsn2'
+              >More than 10 years</option>
+              <option value='C'
+              >C Items/Cheap Items</option>
+              <option value='B'
+              >B Items/Mid Ranged Items</option>
+              <option value='A'
+              >A Items/Expensive Items</option>
+            </select>
+          </div>
+          <Modal go={go} />
+        </div>
+        <div className="card">
+          <div className="table-responsive">
+            {inventoryData?.length > 0 ?
               <table className="table table-striped" style={{ width: "100%", height: "100%" }}>
-                
+
                 <thead className="table__header">
-                  <tr>            
+                  <tr>
                     <th>Sno</th>
                     <th>Item Name</th>
                     <th>Description</th>
@@ -96,13 +111,12 @@ const Inventory = () => {
                   {returnAllInventoryData(inventoryData)}
                 </tbody>
               </table> : <h1>No items found</h1>}
-            </div>
-            </div>
           </div>
         </div>
+      </div>
+    </div>
   );
 };
 
 export default Inventory;
-
 
