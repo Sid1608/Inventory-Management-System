16dfd3ddfa37a685681d1184332c61f1d7f08fad
Sid1608 <akarsiddharth@gmail.com>
2022-01-07 17:06:06 +0530

    changes

--- client/src/modules/Inventory/components/OrderDetailsModal/OrderDetailsModal.js
+++ client/src/modules/Inventory/components/OrderDetailsModal/OrderDetailsModal.js
@@ -16,68 +16,68 @@ const style = {
   borderRadius: 2
 };
 
-function OrderDetailsModal({order}) {
-    const [open, setOpen] = React.useState(false);
-    const [orderData, setOrderData] = React.useState({})
-    const [retrieved, setRetrieved] = React.useState(false)
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => {
-        setOpen(false);
-        setRetrieved(false)
-    }
-    
-    
+export default function OrderHistoryDetailsModal({ order }) {
+  console.log(order);
+  const [open, setOpen] = React.useState(false);
+  const [orderData, setOrderData] = React.useState([])
+  const [retrieved, setRetrieved] = React.useState(false)
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setRetrieved(false)
+  }
 
-    if(open && !retrieved ){
-        (function(order){
-            console.log(order)
-            axios.get(`http://localhost:8080/admin/searchOrder/${order._id}`).then(res => {
-                console.log(res.data)
-                const orderDetails = {
-                    order_by: res.data.user_id.name,
-                    department: res.data.user_id.department,
-                    total_items: res.data.item_count,
-                    items: res.data.issued_items
-                }
-                setOrderData(orderDetails)
-            })
-        })(order)
-        setRetrieved(true)
-    }
+  if (open && !retrieved) {
+    (function (order) {
+      console.log(order)
+      axios.get("http://localhost:8080/admin/searchOrder/" + order._id).then(res => {
+        console.log(res.data)
+        const orderDetails = {
+          total_items: res.data.item_count,
+          items: res.data.issued_items
+        }
+        setOrderData(orderDetails)
+      })
+    })(order)
+    setRetrieved(true)
+  }
 
-    return (
-        <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleOpen}>
-            Details
-        </button>
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-                Order Details
-            </Typography>
-                Order By: {orderData.order_by}
-                <br/>
-                Department: {orderData.department}
-                <br />
-                Total Items: {orderData.total_items}
-                <br />
-                {/* {orderData.items.map((item) => (
-                    <ul>
-                        <li>
-                            Item
-                        </li>
-                    </ul>
-                ))} */}
-            </Box>
-        </Modal>
-        </div>
-    );
+
+  return (
+    <div>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-black-700 rounded"
+        onClick={handleOpen}>
+        Details
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Order Details
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Total Items: {order.issued_items.length}
+            <br />
+            Purpose : {order.remark}
+            <br />
+            <ul>
+              {order.issued_items.map((item) => (
+                <li>
+                  ItemName : {item.item_name} &nbsp;
+                  ItemCount :{item.item_count}  &nbsp;
+                  Description :{item.description}  &nbsp;
+                </li>
+              )
+              )
+              }
+            </ul>
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
-
-export default OrderDetailsModal
--- client/src/modules/Inventory/components/OrderHistoryDetailsModal/OrderHistoryDetailsModal.js
+++ client/src/modules/Inventory/components/OrderHistoryDetailsModal/OrderHistoryDetailsModal.js
@@ -16,7 +16,7 @@ const style = {
   borderRadius: 2
 };
 
-export default function OrderHistoryDetailsModal({order}) {
+export default function OrderHistoryDetailsModal({ order }) {
   console.log(order);
   const [open, setOpen] = React.useState(false);
   const [orderData, setOrderData] = React.useState([])
@@ -27,26 +27,26 @@ export default function OrderHistoryDetailsModal({order}) {
     setRetrieved(false)
   }
 
-  // if (open && !retrieved) {
-  //   (function (order) {
-  //     console.log(order)
-  //     axios.get("http://localhost:8080/admin/searchOrder/" + order._id).then(res => {
-  //       console.log(res.data)
-  //       const orderDetails = {
-  //         total_items: res.data.item_count,
-  //         items: res.data.issued_items
-  //       }
-  //       setOrderData(orderDetails)
-  //     })
-  //   })(order)
-  //   setRetrieved(true)
-  // }
+  if (open && !retrieved) {
+    (function (order) {
+      console.log(order)
+      axios.get("http://localhost:8080/admin/searchOrder/" + order._id).then(res => {
+        console.log(res.data)
+        const orderDetails = {
+          total_items: res.data.item_count,
+          items: res.data.issued_items
+        }
+        setOrderData(orderDetails)
+      })
+    })(order)
+    setRetrieved(true)
+  }
 
 
   return (
     <div>
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-black-700 rounded"
-         onClick={handleOpen}>
+        onClick={handleOpen}>
         Details
       </button>
       <Modal
@@ -62,16 +62,18 @@ export default function OrderHistoryDetailsModal({order}) {
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Total Items: {order.issued_items.length}
             <br />
+            Purpose : {order.remark}
+            <br />
             <ul>
-            {order.issued_items.map((item) => (
-                        <li>
-                            ItemName : {item.item_name} &nbsp;
-                            ItemCount :{item.item_count}  &nbsp;
-                            Description :{item.description}  &nbsp;
-                        </li>
-                )
+              {order.issued_items.map((item) => (
+                <li>
+                  ItemName : {item.item_name} &nbsp;
+                  ItemCount :{item.item_count}  &nbsp;
+                  Description :{item.description}  &nbsp;
+                </li>
               )
-            } 
+              )
+              }
             </ul>
           </Typography>
         </Box>
