58db52a485b42ceced7d31a0cc680255c74e5e88
sid1608 <akarsiddharth@gmail.com>
2022-08-03 18:05:41 +0530

    fix cors

--- server.js
+++ server.js
@@ -14,10 +14,23 @@ app.use(cookieParser())
 //MongoDb Connection
 connectDB();
 //Middlewares
-app.use(cors({
-    origin: 'https://inventory-backend-v1.herokuapp.com/',
+
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+// }));
+const corsConfig = {
     credentials: true,
-}));
+    origin: true,
+};
+app.use(cors(corsConfig));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//  });
+
 app.use(express.json());
 app.use("/auth",authRoutes)
 app.use("/user",userRoutes)
